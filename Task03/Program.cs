3cbb76960f51c48335030a2f62261d550a07862e
tryforeese <stas.desu@yandex.ru>
2023-08-03 18:14:02 +0300

    Выполнил четвертое задание

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int num1;
+        Console.WriteLine("Введите число");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        for (int i = 2; i <= num1; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }       
+    }
+}
