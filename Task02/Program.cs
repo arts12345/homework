c5a752ca9ea6aa0e5ac296b9f6b1234e8a38e348
tryforeese <stas.desu@yandex.ru>
2023-08-03 18:05:38 +0300

    Выполнил третье задание

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int num1;
+        Console.WriteLine("Введите число");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        
+        
+        if (num1 % 2 ==0)
+        {
+            Console.WriteLine($"Число {num1} является чётным");
+        }
+        else  
+        {
+            Console.WriteLine($"Число {num1} не является чётным");
+        }
+        
+    }
+}
