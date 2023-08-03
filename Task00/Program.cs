9155b0d928fafcc87124b55455051f080c9beec5
tryforeese <stas.desu@yandex.ru>
2023-08-03 17:28:30 +0300

    первое задание выполнено

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int num1, num2;
+        Console.WriteLine("Введите первое число");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе число");
+        num2 = Convert.ToInt32(Console.ReadLine());
+        if (num1 > num2)
+        {
+            Console.WriteLine($"Число {num1} больше {num2}");
+        }
+        else
+        {
+            Console.WriteLine($"Число {num2} больше {num1}");
+        }
+
+    }
+}
