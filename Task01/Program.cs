3464be5b4c21247cd7a64e9afc842c99c54b3447
tryforeese <stas.desu@yandex.ru>
2023-08-03 17:59:21 +0300

    Решение второй задачи

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int num1, num2, num3;
+        Console.WriteLine("Введите первое число");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе число");
+        num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите третье число");
+        num3 = Convert.ToInt32(Console.ReadLine());
+        if (num1 > num2 & num1 > num3 )
+        {
+            Console.WriteLine($"Число {num1} больше {num2} и {num3}");
+        }
+        else if (num2 > num1 & num2 > num3)
+        {
+            Console.WriteLine($"Число {num2} больше {num1} и {num3}");
+        }
+        else if  (num3 > num1 & num3 > num2)
+        {
+            Console.WriteLine($"Число {num3} больше {num1} и {num2}");
+        }
+    }
+}
